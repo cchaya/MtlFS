e0efec576ae580d24522f9e881efdf5de369cb79
Stanley Goldman <Stanley.Goldman@gmail.com>
2014-10-24 16:41:21 -0400

    Unit Test using JToken.DeepEquals

--- src/Orchard.Tests.Modules/DesignerTools/Services/ObjectDumperTests.cs
+++ src/Orchard.Tests.Modules/DesignerTools/Services/ObjectDumperTests.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using Orchard.DesignerTools.Services;
 using Orchard.DisplayManagement;
@@ -49,7 +51,17 @@ namespace Orchard.Tests.Modules.DesignerTools.Services
             ObjectDumper.ConvertToJSon(xElement, stringBuilder);
             var json = stringBuilder.ToString();
 
-            Assert.AreEqual("\"name\": \"Model\", \"value\": \"null\"", json);
+            var jObject = new JObject(
+                new JProperty("name", "Model"), 
+                new JProperty("value", "null"));
+
+            ComparareJsonObject(jObject, json);
+        }
+
+        private static void ComparareJsonObject(JObject expectedResult, string json) {
+            var objectDumperJson = JToken.Parse("{" + json + "}");
+
+            Assert.IsTrue(JToken.DeepEquals(expectedResult, objectDumperJson));
         }
 
         [Test]
@@ -62,7 +74,11 @@ namespace Orchard.Tests.Modules.DesignerTools.Services
             ObjectDumper.ConvertToJSon(xElement, stringBuilder);
             var json = stringBuilder.ToString();
 
-            Assert.AreEqual("\"name\": \"Model\", \"value\": \"1337\"", json);
+            var jObject = new JObject(
+                new JProperty("name", "Model"), 
+                new JProperty("value", "1337"));
+
+            ComparareJsonObject(jObject, json);
         }
 
         [Test]
@@ -75,7 +91,11 @@ namespace Orchard.Tests.Modules.DesignerTools.Services
             ObjectDumper.ConvertToJSon(xElement, stringBuilder);
             var json = stringBuilder.ToString();
 
-            Assert.AreEqual("\"name\": \"Model\", \"value\": \"True\"", json);
+            var jObject = new JObject(
+                new JProperty("name", "Model"), 
+                new JProperty("value", "True"));
+
+            ComparareJsonObject(jObject, json);
         }
 
         [Test]
@@ -88,7 +108,11 @@ namespace Orchard.Tests.Modules.DesignerTools.Services
             ObjectDumper.ConvertToJSon(xElement, stringBuilder);
             var json = stringBuilder.ToString();
 
-            Assert.AreEqual("\"name\": \"Model\", \"value\": \"&quot;Never gonna give you up&quot;\"", json);
+            var jObject = new JObject(
+                new JProperty("name", "Model"),
+                new JProperty("value", "&quot;Never gonna give you up&quot;"));
+
+            ComparareJsonObject(jObject, json);
         }
 
         [Test]
@@ -119,12 +143,22 @@ namespace Orchard.Tests.Modules.DesignerTools.Services
             ObjectDumper.ConvertToJSon(xElement, stringBuilder);
             var json = stringBuilder.ToString();
 
-            Assert.AreEqual("\"name\": \"Model\", " +
-                            "\"value\": \"Int32[]\", " +
-                            "\"children\": [" +
-                                "{ \"name\": \"[0]\", \"value\": \"1\" }, " +
-                                "{\"name\": \"[1]\", \"value\": \"2\" }, " +
-                                "{\"name\": \"[2]\", \"value\": \"3\" }]", json);
+            var jObject = new JObject(
+                new JProperty("name", "Model"), 
+                new JProperty("value", "Int32[]"), 
+                new JProperty("children", new JArray(
+                    new JObject(
+                        new JProperty("name", "[0]"), 
+                        new JProperty("value", "1")),
+                    new JObject(
+                        new JProperty("name", "[1]"), 
+                        new JProperty("value", "2")),
+                    new JObject(
+                        new JProperty("name", "[2]"), 
+                        new JProperty("value", "3"))
+                    )));
+
+            ComparareJsonObject(jObject, json);
         }
 
         [Test]
@@ -154,12 +188,22 @@ namespace Orchard.Tests.Modules.DesignerTools.Services
             ObjectDumper.ConvertToJSon(xElement, stringBuilder);
             var json = stringBuilder.ToString();
 
-            Assert.AreEqual("\"name\": \"Model\", " +
-                            "\"value\": \"Dictionary&lt;String, Int32&gt;\", " +
-                            "\"children\": [" +
-                                "{ \"name\": \"[&quot;One&quot;]\", \"value\": \"1\" }, " +
-                                "{\"name\": \"[&quot;Two&quot;]\", \"value\": \"2\" }, " +
-                                "{\"name\": \"[&quot;Three&quot;]\", \"value\": \"3\" }]", json);
+            var jObject = new JObject(
+                new JProperty("name", "Model"),
+                new JProperty("value", "Dictionary&lt;String, Int32&gt;"),
+                new JProperty("children", new JArray(
+                    new JObject(
+                        new JProperty("name", "[&quot;One&quot;]"),
+                        new JProperty("value", "1")),
+                    new JObject(
+                        new JProperty("name", "[&quot;Two&quot;]"),
+                        new JProperty("value", "2")),
+                    new JObject(
+                        new JProperty("name", "[&quot;Three&quot;]"),
+                        new JProperty("value", "3"))
+                    )));
+
+            ComparareJsonObject(jObject, json);
         }
 
         [Test]
@@ -195,13 +239,26 @@ namespace Orchard.Tests.Modules.DesignerTools.Services
             ObjectDumper.ConvertToJSon(xElement, stringBuilder);
             var json = stringBuilder.ToString();
 
-            Assert.AreEqual("\"name\": \"Model\", " +
-                            "\"value\": \"TestObject\", " +
-                            "\"children\": [" +
-                                "{ \"name\": \"SomeInteger\", \"value\": \"1337\" }, " +
-                                "{\"name\": \"SomeBoolean\", \"value\": \"True\" }, " +
-                                "{\"name\": \"SomeString\", \"value\": \"&quot;Never gonna give you up&quot;\" }, " +
-                                "{\"name\": \"ChildObject\", \"value\": \"null\" }]", json);
+            var jObject = new JObject(
+                new JProperty("name", "Model"),
+                new JProperty("value", "TestObject"),
+                new JProperty("children", new JArray(
+                    new JObject(
+                        new JProperty("name", "SomeInteger"),
+                        new JProperty("value", "1337")),
+                    new JObject(
+                        new JProperty("name", "SomeBoolean"),
+                        new JProperty("value", "True")),
+                    new JObject(
+                        new JProperty("name", "SomeString"),
+                        new JProperty("value", "&quot;Never gonna give you up&quot;")),
+                    new JObject(
+                        new JProperty("name", "ChildObject"),
+                        new JProperty("value", "null")
+                    )
+                )));
+
+            ComparareJsonObject(jObject, json);
         }
 
         [Test]
@@ -224,12 +281,22 @@ namespace Orchard.Tests.Modules.DesignerTools.Services
             ObjectDumper.ConvertToJSon(xElement, stringBuilder);
             var json = stringBuilder.ToString();
 
-            Assert.AreEqual("\"name\": \"Model\", " +
-                            "\"value\": \"TestObject\", " +
-                            "\"children\": [" +
-                                "{ \"name\": \"SomeInteger\", \"value\": \"1337\" }, " +
-                                "{\"name\": \"SomeBoolean\", \"value\": \"True\" }, " +
-                                "{\"name\": \"SomeString\", \"value\": \"&quot;Never gonna give you up&quot;\" }]", json);
+            var jObject = new JObject(
+                new JProperty("name", "Model"),
+                new JProperty("value", "TestObject"),
+                new JProperty("children", new JArray(
+                    new JObject(
+                        new JProperty("name", "SomeInteger"),
+                        new JProperty("value", "1337")),
+                    new JObject(
+                        new JProperty("name", "SomeBoolean"),
+                        new JProperty("value", "True")),
+                    new JObject(
+                        new JProperty("name", "SomeString"),
+                        new JProperty("value", "&quot;Never gonna give you up&quot;"))
+                    )));
+
+            ComparareJsonObject(jObject, json);
         }
 
         [Test]
@@ -252,20 +319,41 @@ namespace Orchard.Tests.Modules.DesignerTools.Services
             ObjectDumper.ConvertToJSon(xElement, stringBuilder);
             var json = stringBuilder.ToString();
 
-            Assert.AreEqual("\"name\": \"Model\", " +
-                            "\"value\": \"TestObject\", " +
-                            "\"children\": [" +
-                            "{ \"name\": \"SomeInteger\", \"value\": \"1337\" }, " +
-                            "{\"name\": \"SomeBoolean\", \"value\": \"True\" }, " +
-                            "{\"name\": \"SomeString\", \"value\": \"&quot;Never gonna give you up&quot;\" }, " +
-                            "{" +
-                                "\"name\": \"ChildObject\", " +
-                                "\"value\": \"TestObject\", " +
-                                "\"children\": [" +
-                                    "{ \"name\": \"SomeInteger\", \"value\": \"58008\" }, " +
-                                    "{\"name\": \"SomeBoolean\", \"value\": \"False\" }, " +
-                                    "{\"name\": \"SomeString\", \"value\": \"&quot;Never gonna let you down&quot;\" }, " +
-                                    "{\"name\": \"ChildObject\", \"value\": \"null\" }] }]", json);
+            var jObject = new JObject(
+                new JProperty("name", "Model"),
+                new JProperty("value", "TestObject"),
+                new JProperty("children", new JArray(
+                    new JObject(
+                        new JProperty("name", "SomeInteger"),
+                        new JProperty("value", "1337")),
+                    new JObject(
+                        new JProperty("name", "SomeBoolean"),
+                        new JProperty("value", "True")),
+                    new JObject(
+                        new JProperty("name", "SomeString"),
+                        new JProperty("value", "&quot;Never gonna give you up&quot;")),
+                    new JObject(
+                        new JProperty("name", "ChildObject"),
+                        new JProperty("value", "TestObject"),
+                        new JProperty("children", new JArray(
+                            new JObject(
+                                new JProperty("name", "SomeInteger"),
+                                new JProperty("value", "58008")),
+                            new JObject(
+                                new JProperty("name", "SomeBoolean"),
+                                new JProperty("value", "False")),
+                            new JObject(
+                                new JProperty("name", "SomeString"),
+                                new JProperty("value", "&quot;Never gonna let you down&quot;")),
+                            new JObject(
+                                new JProperty("name", "ChildObject"),
+                                new JProperty("value", "null")
+                            )
+                        ))
+                    )
+                )));
+
+            ComparareJsonObject(jObject, json);
         }
     }
 }
